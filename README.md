cc4adcbf551ff95fb78cbbb26537ff60cab573a7
Anuj Gupta <anujgupta@Anujs-MacBook-Air.local>
2021-07-02 09:43:38 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# udayy-assignment
+Install Chrome or Mozila firefox browser.
+
+1. Open the folder shared on email.
+2. Click to launch the main_page.html.
+3. You will be directed to the login page, please enter - user name - Udayy, Password - anuj_gupta.
+4. Now, you will see the TextBox1 & TextBox2, please enter some URLs there like https://ruralspark.com & then click outside of that box, it will open the that url in the box below.
+
+<———Code files————>
+
+1. To check the code files, please install VS code.
+2. Now, after installation, open the folder in VS code & you can find all the files over there.
+
+<———Thank you———>
---
+++ src.js
@@ -0,0 +1,39 @@
+function changeEvent1(event) {
+  const url = event.target.value;
+  document.getElementById("displayURL-1").src = url;
+  console.log(url);
+}
+function changeEvent2(event) {
+  const url = event.target.value;
+  document.getElementById("displayURL-2").src = url;
+  console.log(url);
+}
+// function logOut(lout) {
+//   document;
+// }
+// function getData() {
+//   let url = this.refs.searchFeed.value;
+//   this.refs.searchFeed.value = "";
+
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .catch((error) => {
+//       console.log("error", error);
+//       reject(error);
+//     });
+// }
+
+//Love - Workwise was  good last couple of weeks, I was busy mostly full time.
+//Love - Getting some new friends.
+//Lack - I couldn't work more on component searching, I was a bit lazy on that.
+//lack - Was mostly related to losing connection with friends
+//learn - mostly busy with the course which I have subscribed.
+
+//Love Mauro - Spain going into final.
+//Lack about Shagun, he is getting old after seeing his family.
+//Learn - Sienna is progressing on SixSigma.
